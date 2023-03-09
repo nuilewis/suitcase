31164b7738a7c90460810a897470e80972229601
Redeem Grimm <45304978+Redeem-Grimm-Satoshi@users.noreply.github.com>
2023-03-09 14:50:05 +0100

    Test Bug
    CircleCI Build Test

--- src/main/java/org/opendatakit/suitcase/Suitcase.java
+++ src/main/java/org/opendatakit/suitcase/Suitcase.java
@@ -16,7 +16,11 @@ import static org.opendatakit.suitcase.ui.LayoutConsts.WINDOW_WIDTH;
 public class Suitcase {
   private static final String SUITCASE_TITLE = "Suitcase";
   public static void main(String[] args) {
-    int retCode = SuitcaseSwingWorker.okCode;
+    
+    //Test Bug
+    TEST BUG
+    
+    int retCode = SuitcaseSwingWorker.okCode
     if (args.length > 0) {
       retCode = new SuitcaseCLI(args).startCLI();
       System.exit(retCode);
